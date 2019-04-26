59de58a4ba753e99e8445b1493ec63ed2be1b3db
pasha <4pasha.com@gmail.com>
2019-04-26 23:08:06 +0300

    add new dependency by heroku add requestMapping in main class

--- src/main/java/compare/site/SiteApplication.java
+++ src/main/java/compare/site/SiteApplication.java
@@ -3,12 +3,33 @@ package compare.site;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.stereotype.Controller;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SiteApplication {
 
+
+    @RequestMapping("/hello")
+    String hello(Map<String, Object> model) {
+        RelativisticModel.select();
+        Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+        model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+        return "hello";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
+
     }
+
 
 }
