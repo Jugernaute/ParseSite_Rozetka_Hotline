680da83ea4eb43270ec882da55de955d0fae46df
pasha <4pasha.com@gmail.com>
2019-04-28 18:52:52 +0300

    db heroku

--- src/main/java/compare/site/SiteApplication.java
+++ src/main/java/compare/site/SiteApplication.java
@@ -1,74 +1,21 @@
 package compare.site;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 
 import static javax.measure.unit.SI.KILOGRAM;
 import javax.measure.quantity.Mass;
-import javax.sql.DataSource;
-
 import org.jscience.physics.model.RelativisticModel;
 import org.jscience.physics.amount.Amount;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SiteApplication {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-//    @Bean
-//    public DataSource dataSource() throws SQLException {
-//        if (dbUrl == null || dbUrl.isEmpty()) {
-//            return new HikariDataSource();
-//        } else {
-//            HikariConfig config = new HikariConfig();
-//            config.setJdbcUrl(dbUrl);
-//            return new HikariDataSource(config);
-//        }
-//    }
-
-
-
-
-    @RequestMapping("/db")
-    String db(Map<String, Object> model, DataSource dataSource) {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-            ArrayList<String> output = new ArrayList<String>();
-            while (rs.next()) {
-                output.add("Read from DB: " + rs.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "hello";
-
-        } catch (Exception e) {
-            model.put("message", e.getMessage());
-            return "error";
-        }
-    }
-
-
 
 
     @RequestMapping("/hello")
